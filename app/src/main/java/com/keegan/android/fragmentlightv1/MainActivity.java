87277119e9b5478f2b536c10a26281f3b16f68d5
Keegan <kgm1328@live.missouristate.edu>
2022-10-29 18:55:49 -0500

    Added transaction for fragments

--- app/src/main/java/com/keegan/android/fragmentlightv1/MainActivity.java
+++ app/src/main/java/com/keegan/android/fragmentlightv1/MainActivity.java
@@ -14,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if(fragmentManager.findFragmentById(R.id.leftFragment) == null)
+        {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            LeftFragment leftFragment = new LeftFragment();
+            transaction.add(R.id.leftFragment, leftFragment);
+            transaction.commit();
+        }
+
+        if(fragmentManager.findFragmentById(R.id.rightFragment) == null)
+        {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            RightFragment rightFragment = new RightFragment();
+            transaction.add(R.id.rightFragment, rightFragment);
+            transaction.commit();
+        }
     }
 }
