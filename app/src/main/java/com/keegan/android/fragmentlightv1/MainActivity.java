0a206bba512bf90d32bc602b949d99f4d1946508
Keegan <kgm1328@live.missouristate.edu>
2022-10-30 15:30:57 -0500

    Made update to finish passing data between fragments

--- app/src/main/java/com/keegan/android/fragmentlightv1/MainActivity.java
+++ app/src/main/java/com/keegan/android/fragmentlightv1/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,20 +18,31 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
-        if(fragmentManager.findFragmentById(R.id.leftFragment) == null)
-        {
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
-            LeftFragment leftFragment = new LeftFragment();
-            transaction.add(R.id.leftFragment, leftFragment);
-            transaction.commit();
-        }
-
         if(fragmentManager.findFragmentById(R.id.rightFragment) == null)
         {
             FragmentTransaction transaction = fragmentManager.beginTransaction();
             RightFragment rightFragment = new RightFragment();
             transaction.add(R.id.rightFragment, rightFragment);
             transaction.commit();
+        }
+    }
+
+    public void updateLight(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        RightFragment rightFragment = (RightFragment) fragmentManager.findFragmentById(R.id.rightFragment);
+        View rightFragmentView = rightFragment.getView();
+        TextView lightButton = (TextView) rightFragmentView.findViewById(R.id.traffic_light);
+
+        if(lightButton.getText().toString().equals("Red Light"))
+        {
+            rightFragment.setGreenLight();
+        }
+        else if(lightButton.getText().toString().equals("Green Light"))
+        {
+            rightFragment.setYellowLight();
+        }
+        else{       //yellow light
+            rightFragment.setRedLight();
         }
     }
 }
--- app/src/main/java/com/keegan/android/fragmentlightv1/RightFragment.java
+++ app/src/main/java/com/keegan/android/fragmentlightv1/RightFragment.java
@@ -1,23 +1,39 @@
 package com.keegan.android.fragmentlightv1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class RightFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_right, container, false);
     }
 
-    public void onStart(){
-        super.onStart();
+    public void setRedLight(){
         View fragmentView = getView();
-        MainActivity fragmentActivity = (MainActivity) getActivity();
+        TextView trafficLight = (TextView) fragmentView.findViewById(R.id.traffic_light);
+        trafficLight.setText(R.string.red_light);
+        trafficLight.setBackgroundColor(Color.RED);
+    }
+
+    public void setGreenLight(){
+        View fragmentView = getView();
+        TextView trafficLight = (TextView) fragmentView.findViewById(R.id.traffic_light);
+        trafficLight.setText(R.string.green_light);
+        trafficLight.setBackgroundColor(Color.GREEN);
+    }
+
+    public void setYellowLight(){
+        View fragmentView = getView();
+        TextView trafficLight = (TextView) fragmentView.findViewById(R.id.traffic_light);
+        trafficLight.setText(R.string.yellow_light);
+        trafficLight.setBackgroundColor(Color.YELLOW);
     }
 }
